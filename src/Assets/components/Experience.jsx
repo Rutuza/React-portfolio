7632f2ab146721f3eb9dce35e48330473cb31354
Rutuza <dalal.jyotika05@gmail.com>
2023-08-17 20:50:39 +0530

    older changes

--- src/Assets/components/Experience.jsx
+++ src/Assets/components/Experience.jsx
@@ -8,23 +8,29 @@ function Experience() {
         <div className="line"></div>
     </div>
     <div className='exp-panel'>
-        <div className='std'>
-            <div className="year"><b>01 Jul - 31 Sep '22</b></div>
+        <div className="row">
+            <div className='year'><b>01Jul - 31Sep '23</b></div>
             <div className='intern-type'>
-                <h3>Frontend Development Intern</h3>
-                <div className='grade'>hjdgfjgjdh</div>
+                <h3>Frontend Development</h3>
+                <div></div>
+            </div>
+        </div>
+        <div className="row">
+            <div className='year'><b>01Jul - 05Aug '23</b></div>
+            <div className='intern-type'>
+                <h3>Web Designing intern</h3>
+                <div></div>
+            </div>
+        </div>
+        <div className="row">
+            <div className='year'><b>Present</b></div>
+            <div className='intern-type'>
+                <h3>Fullstack MERN intern</h3>
+                <div></div>
             </div>
         </div>
     </div>
-    <div className='exp-panel'>
-        <div className='std'>
-            <div className="year"><b>01 Jul - 31 Sep '22</b></div>
-            <div className='intern-type'>
-                <h3>Web desiging Intern</h3>
-                <div className='grade'>hjdgfjgjdh</div>
-            </div>
-        </div>
-    </div>
+    
     
 </div>
   );
--- src/Assets/pages/Home.jsx
+++ src/Assets/pages/Home.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Intro from '../components/Intro';
-import Ellipse from '../components/Ellipse';
-import Follow from '../components/Follow';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Intro from "../components/Intro";
+import Ellipse from "../components/Ellipse";
+import Follow from "../components/Follow";
 function Home() {
   return (
     <div>
       <Navbar />
-      <Intro/>
-      <Ellipse/>
+      <Intro />
+      <Ellipse />
       <Follow />
     </div>
   );
