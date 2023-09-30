fae6023eede0dff8731d746fa943278c5be0b5e1
Rutuza <dalal.jyotika05@gmail.com>
2023-09-30 23:57:38 +0530

    changing button

--- src/Assets/components/Skills.jsx
+++ src/Assets/components/Skills.jsx
@@ -6,10 +6,10 @@ function Skills() {
       <h1>Skills</h1>
       <div className="content">
         <div className="left">
-          <h1>My creative skills and knowledge about technology</h1>
+          <h2>My creative skills and knowledge about technology</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dicta adipisci asperiores laboriosam soluta quis omnis nam beatae molestias optio provident, quasi libero! Suscipit eos perspiciatis eaque commodi dolorum aliquam atque provident harum distinctio alias, saepe quos nihil voluptates quo, laudantium corrupti, ratione deserunt voluptatem iure enim maxime? Illo rem dolor officiis voluptatibus ea libero, id nam maiores debitis, doloremque cupiditate repellendus vitae corrupti reiciendis blanditiis sint recusandae, nisi ipsum. Dolores, ex eos. Nam, incidunt voluptatum fugiat eveniet cumque unde voluptatem! Officiis ipsa illum magni omnis dolorem cupiditate vero dolorum nostrum hic, delectus maxime voluptatum mollitia voluptatem eaque ab, impedit nam itaque, maiores nesciunt odio temporibus autem aperiam! A, veniam sed at cumque adipisci accusantium corporis assumenda esse corrupti nemo!</p>
 
-          <a href="#">Read More</a>
+          <button className="readmore-btn"><a href="#">Read More</a></button>
         </div>
 
         <div className="right">
