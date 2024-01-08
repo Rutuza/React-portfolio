bcfaef33426def9e3e9cf21c14cb06763f3e45ef
Rutuza <dalal.jyotika05@gmail.com>
2024-01-08 15:34:06 +0530

    routing

--- src/Assets/pages/Homepage2.jsx
+++ src/Assets/pages/Homepage2.jsx
@@ -3,19 +3,23 @@ import {useTypewriter, Cursor} from 'react-simple-typewriter';
 // import homebg from '../SVG/homepage bg.svg';
 import Navbar from '../components/Navbar';
 function Homepage2() {
-    const {text} = useTypewriter({
+    const [text] = useTypewriter({
         words: ['Jyotika Dalal', 'Developer', 'UI/UX Designer', 'Photographer', 'Eppigrammatist'],
         loop: {},
         typeSpeed: 120,
-    })
+    });
+
   return (
     <div id='home'>
         <div>
             <Navbar/>
             <div id="inn-container">
                 <div className='lhs'>
-                    <h1>Hi <br /> I am Jyomtika bai<span id='text'>{text}</span>
-                    <span id='cursor'><Cursor cursorStyle='|'/></span></h1>
+                    <h1>Hi <br /> I am {' '}
+                        <span>
+                            {text}
+                        </span>
+                    <span id='cursor'><Cursor/></span></h1>
                     <h5>Design and code what you think</h5>
                 </div>
                 <div className='rhs'></div>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 import './Scss/index.scss'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Assets/pages/Homepage2';
+import About from './Assets/pages/AboutPage';
+import Skills from './Assets/pages/SkillsPage';
+import Work from './Assets/pages/WorkPage';
+
+export default function App(){
+  return(
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='/About' element={<About />}/>
+          <Route path='/Skills' element={<Skills />}/>
+          <Route path='/Work' element={<Work />}/>
+        </Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 reportWebVitals();
